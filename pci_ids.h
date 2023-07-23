61e3013aa18cf25c8652b2d66b7888d4d70768cd
Vasily Galkin <galkin-vv@ya.ru>
2023-07-23 04:41:03 +0300

    add id for WX5100

--- pci_ids.h
+++ pci_ids.h
@@ -11,6 +11,7 @@
  */
 static const struct pci_device_id pciidlist[] = {
     {0x1002, 0x67df, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_POLARIS10}, //470-590
+    {0x1002, 0x67c7, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_POLARIS10}, //wx5100
     {0x1002, 0x6fdf, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_POLARIS10}, //580 2048Sp/590GME
     {0x1002, 0x67FF, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_POLARIS11},     // RX560
     {0x1002, 0x687F, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_VEGA10},        // Vega
